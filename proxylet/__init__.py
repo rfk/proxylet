d2b9ff4c59dba82f751071baa293c21234866a07
Ryan Kelly <ryan@rfk.id.au>
2009-03-18 11:19:04 +1100

    bump version number

--- proxylet/__init__.py
+++ proxylet/__init__.py
@@ -37,7 +37,7 @@ to a private fileserver, and return 404 for any other paths:
 
 __ver_major__ = 0
 __ver_minor__ = 1
-__ver_patch__ = 0
+__ver_patch__ = 1
 __ver_sub__ = ""
 __version__ = "%d.%d.%d%s" % (__ver_major__,__ver_minor__,
                               __ver_patch__,__ver_sub__)
